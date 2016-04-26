4af6fdc7e7b3aced23d8896fb15374afa731dda8
Kaleb Elwert <belak@coded.io>
2016-04-26 16:52:55 -0700

    Move indexByte to an internal package
    This allows both the ctcp package and the irc package to share the code, which
    should allow the build to succeed for 1.1 again.

--- internal/strings_legacy.go
+++ internal/strings_legacy.go
@@ -9,10 +9,10 @@
 //
 // This code may be removed when Wheezy is no longer supported.
 
-package irc
+package internal
 
-// indexByte implements strings.IndexByte for Go versions < 1.2.
-func indexByte(s string, c byte) int {
+// IndexByte implements strings.IndexByte for Go versions < 1.2.
+func IndexByte(s string, c byte) int {
 	for i := range s {
 		if s[i] == c {
 			return i
